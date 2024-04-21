e1cf7abc5f240ecc7b3d4bd0d40869b498b48ac4
Aaron Abbott <eragon3233@gmail.com>
2024-04-21 16:28:30 +1000

    added setters getters and other methods

--- frontend/src/classes/player.ts
+++ frontend/src/classes/player.ts
@@ -9,7 +9,8 @@ export class Player {
     //private abilities: Ability[];
     //the winstreak is how many DUELS a player has won in a row. not the matches.
     private winstreak: number;
-    private spectators: number;
+    private winstreakHigh: number;
+    private numSpectators: number;
     private isBot: boolean;
 
     constructor(name: string, id: number, isBot: boolean) {
@@ -18,7 +19,60 @@ export class Player {
         this.isSpectating = false;
         //this.abilities = [];
         this.winstreak = 0;
-        this.spectators = 0;
+        this.winstreakHigh = 0;
+        this.numSpectators = 0;
         this.isBot = isBot;
+    }
+
+    //getters
+    public getName(): string {
+        return this.name;
+    }
+    public getId(): number {
+        return this.id;
+    }
+    public getIsSpectating(): boolean {
+        return this.isSpectating;
+    }
+    public getWinstreak(): number {
+        return this.winstreak;
+    }
+    public getNumSpectators(): number {
+        return this.numSpectators;
+    }
+    public getIsBot(): boolean {
+        return this.isBot;
+    }
+
+    //setters
+    public setIsSpectating(isSpectating: boolean) {
+        this.isSpectating = isSpectating;
+    }
+    public setWinstreak(winstreak: number) {
+        this.winstreak = winstreak;
+    }
+    public setNumSpectators(numSpectators: number) {
+        this.numSpectators = numSpectators;
+    }
+    public setIsBot(isBot: boolean) {
+        this.isBot = isBot;
+    }
+
+    //additional methods
+    public incrementWinstreak() {
+        this.winstreak++;
+        if (this.winstreak > this.winstreakHigh) {
+            this.winstreakHigh = this.winstreak;
+        }
+    }
+    public resetWinstreak() {
+        this.winstreak = 0;
+    }
+
+    public incrementNumSpectators(newSpec: number) {
+        this.numSpectators += newSpec;
+    }
+    public decrementNumSpectators(lessSpec: number) {
+        this.numSpectators -= lessSpec;
     }
 }
