260663e15420a416bbd3543f40a567bc84dc5aef
Aaron Abbott <eragon3233@gmail.com>
2024-04-21 16:34:22 +1000

    prettyfied using prettier

--- frontend/src/classes/player.ts
+++ frontend/src/classes/player.ts
@@ -1,7 +1,6 @@
 //Very basic player class, can be expanded if needed
 //Author: Team Buddy (4), Aaron Abbott 32520441
 
-
 export class Player {
     private name: string;
     private id: number;
@@ -12,67 +11,67 @@ export class Player {
     private winstreakHigh: number;
     private numSpectators: number;
     private isBot: boolean;
-
+  
     constructor(name: string, id: number, isBot: boolean) {
-        this.name = name;
-        this.id = id;
-        this.isSpectating = false;
-        //this.abilities = [];
-        this.winstreak = 0;
-        this.winstreakHigh = 0;
-        this.numSpectators = 0;
-        this.isBot = isBot;
+      this.name = name;
+      this.id = id;
+      this.isSpectating = false;
+      //this.abilities = [];
+      this.winstreak = 0;
+      this.winstreakHigh = 0;
+      this.numSpectators = 0;
+      this.isBot = isBot;
     }
-
+  
     //getters
     public getName(): string {
-        return this.name;
+      return this.name;
     }
     public getId(): number {
-        return this.id;
+      return this.id;
     }
     public getIsSpectating(): boolean {
-        return this.isSpectating;
+      return this.isSpectating;
     }
     public getWinstreak(): number {
-        return this.winstreak;
+      return this.winstreak;
     }
     public getNumSpectators(): number {
-        return this.numSpectators;
+      return this.numSpectators;
     }
     public getIsBot(): boolean {
-        return this.isBot;
+      return this.isBot;
     }
-
+  
     //setters
     public setIsSpectating(isSpectating: boolean) {
-        this.isSpectating = isSpectating;
+      this.isSpectating = isSpectating;
     }
     public setWinstreak(winstreak: number) {
-        this.winstreak = winstreak;
+      this.winstreak = winstreak;
     }
     public setNumSpectators(numSpectators: number) {
-        this.numSpectators = numSpectators;
+      this.numSpectators = numSpectators;
     }
     public setIsBot(isBot: boolean) {
-        this.isBot = isBot;
+      this.isBot = isBot;
     }
-
+  
     //additional methods
     public incrementWinstreak() {
-        this.winstreak++;
-        if (this.winstreak > this.winstreakHigh) {
-            this.winstreakHigh = this.winstreak;
-        }
+      this.winstreak++;
+      if (this.winstreak > this.winstreakHigh) {
+        this.winstreakHigh = this.winstreak;
+      }
     }
     public resetWinstreak() {
-        this.winstreak = 0;
+      this.winstreak = 0;
     }
-
+  
     public incrementNumSpectators(newSpec: number) {
-        this.numSpectators += newSpec;
+      this.numSpectators += newSpec;
     }
     public decrementNumSpectators(lessSpec: number) {
-        this.numSpectators -= lessSpec;
+      this.numSpectators -= lessSpec;
     }
-}
+  }
