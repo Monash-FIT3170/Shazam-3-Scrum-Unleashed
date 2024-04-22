750cb4886073bb7b3c805ccd47c298310399dda0
Aaron Abbott <40223320+Technolord3233@users.noreply.github.com>
2024-04-22 12:59:01 +1000

    created a tournament manager (#24)

---
+++ frontend/src/classes/tournamentManager.ts
@@ -0,0 +1,57 @@
+//tournament manager class
+//tournament class which takes in an array of Player objects, and then constructs the first level of the tournament bracket.
+//has functions to help run the tournament, such as constructing the next level of the tournament after people win their matches. can also return specific brackets to display
+
+import { Player } from "./player";
+
+export class TournamentManager {
+  private tournamentBrackets: Array<Array<Player>>;
+
+  constructor(players: Array<Player>) {
+    this.tournamentBrackets = [];
+    this.shufflePlayers(players);
+    this.fillWithBots(players);
+  }
+
+  //getters
+  public getRoundNumber(): number {
+    return this.tournamentBrackets.length;
+  }
+
+  public getBracket(round: number): Array<Player> {
+    return this.tournamentBrackets[round];
+  }
+
+  //misc methods
+
+  //method to fill the match with bots if the number of players is not a power of 2
+  private fillWithBots(players: Array<Player>) {
+    //this function to find the next power of 2 was taken off stack overflow: https://stackoverflow.com/a/466256
+    const nextPowerOf2: number = Math.pow(
+      2,
+      Math.ceil(Math.log(players.length) / Math.log(2)),
+    );
+    const numBots: number = nextPowerOf2 - players.length;
+    //assuming player IDs are just from 0 to n-1
+    let nextID = players.length;
+    //fill every 2nd spot with a bot
+    const round1: Array<Player> = [];
+    for (let i = 0; i < players.length; i++) {
+      round1.push(players[i]);
+      if (i < numBots) {
+        round1.push(new Player("🤖 Bot #" + i, nextID, true));
+        nextID++;
+      }
+    }
+    this.tournamentBrackets.push(round1);
+  }
+
+  private shufflePlayers(players: Array<Player>): Array<Player> {
+    //use the fisher-yates shuffle algorithm, with the in place modification by Durstenfeld
+    for (let i = players.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      [players[i], players[rand]] = [players[rand], players[i]];
+    }
+    return players;
+  }
+}
