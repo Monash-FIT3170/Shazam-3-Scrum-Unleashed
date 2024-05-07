b4a92e97fa0725032454ac832635882e8ea3fdd9
Suryadeep Singh <suryadeep.singh098@gmail.com>
2024-05-07 20:48:24 +1000

    added arguments to semi circle component

--- client/src/pages/gameRoundScreen.tsx
+++ client/src/pages/gameRoundScreen.tsx
@@ -3,9 +3,10 @@ const GameRoundScreen = () => {
   return (
     
       <div className="flex justify-center items-start">
-     
-
-        <SemiCircle3dots/>   
+        <div>
+        <SemiCircle3dots variant="text" text="make your move" angle="180"/>
+        <h1 className="text-white"> TIMES RUNNING OUT!</h1>
+        </div>   
         
 {/*<h1 className="text-white font-bold mt-6 uppercase"> Game Code : {gameCode}</h1>*/}
       </div>
