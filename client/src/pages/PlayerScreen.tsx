43b81e71acdb0f9c7f7bb467b73618ed2fbf00d2
Levi Kogan <128117470+lkog77@users.noreply.github.com>
2024-05-10 14:05:50 +1000

    Feature/winning UI (#43)
    * Tournament Player Winner UI
    
    * Updates to Player winning UI
    
    * Added end of tournament UI
    
    * Commented out unused import. Updated div to use tailwind css

--- client/src/pages/PlayerScreen.tsx
+++ client/src/pages/PlayerScreen.tsx
@@ -1,12 +1,22 @@
 import DisplayLogo from "../components/DisplayLogo";
+import WinnerPlayer from "../components/WinnerPlayer";
+//import EndTournament from "../components/EndTournament";
+
+// For testing purposes I've added both componenets to Player Screen.
+// Components will be conditionally rendered based on the game state.
 
 const PlayerScreen = () => {
   return (
-    <div>
+    <div className="overflow-hidden pt-12">
       <div>
         <DisplayLogo />
       </div>
-      {/*<h1 className="text-white font-bold mt-6 uppercase"> Game Code : {gameCode}</h1>*/}
+      {/* <div className="mt-5">
+        <EndTournament player={"PLAYER X"}/> 
+      </div> */}
+      <div className="mt-5">
+        <WinnerPlayer />
+      </div>
     </div>
   );
 };
