9f20ae0a3264b3c02089dd1eff61c0300402b311
SANDY_SENG <ssen0027@student.monash.edu>
2024-05-10 17:57:41 +1000

    Ran automated linting test

--- client/src/components/SpectatorCount.tsx
+++ client/src/components/SpectatorCount.tsx
@@ -1,18 +1,18 @@
 import eyeIcon from "../assets/EyeIcon.svg";
 
 type SpectatorCountProps = {
-    count: number
-}
+  count: number;
+};
 
-const SpectatorCount = ({count}: SpectatorCountProps) => {
-    return (
-        <div className='fixed bottom-5 right-7 text-4xl text-white font-bold items-center flex'>
-            <div className='float-left scale-90'>
-                <img src={eyeIcon} alt="EyeIcon" />
-            </div>
-            <p className='float-right ml-3'>{count}</p>
-        </div>
-    );
+const SpectatorCount = ({ count }: SpectatorCountProps) => {
+  return (
+    <div className="fixed bottom-5 right-7 text-4xl text-white font-bold items-center flex">
+      <div className="float-left scale-90">
+        <img src={eyeIcon} alt="EyeIcon" />
+      </div>
+      <p className="float-right ml-3">{count}</p>
+    </div>
+  );
 };
 
 export default SpectatorCount;
--- client/src/pages/MatchWinScreen.tsx
+++ client/src/pages/MatchWinScreen.tsx
@@ -6,48 +6,60 @@ import StarsInfoPlayer from "../components/StarsInfoPlayer";
 import StarsInfoOpponent from "../components/StarsInfoOpponent";
 
 type MatchWinScreenProps = {
-    playerScore: number,
-    opponentScore: number,
-    playerName: string,
-    opponentName: string,
-    spectatorCount: number
-}
+  playerScore: number;
+  opponentScore: number;
+  playerName: string;
+  opponentName: string;
+  spectatorCount: number;
+};
 
-const MatchWinScreen = ({playerScore = 3, opponentScore = 2, playerName = "SPONGEBOB", opponentName = "PATRICK", spectatorCount = 32}: MatchWinScreenProps) => {
-    return (
-        <div>
-            <div className='block md:hidden'>
-                <PlayerName1 name={opponentName}/>
-            </div>
-            <div className='hidden md:block'>
-                <StarsInfoOpponent opponentName={opponentName} score={opponentScore}/>
-            </div>
-            <div className='fixed top-[5%] mx-auto max-w-max inset-x-0 scale-[0.85] sm:scale-[0.85] md:scale-[0.875] lg:scale-[0.90]'>
-                <img src={plainTrophy} alt="PlainTrophy" />
-            </div>
-            <div className='text-white font-bold text-6xl fixed top-[39%] mx-auto max-w-max inset-x-0'>
-                <h1>YOU WON!</h1>
-            </div>
-            <div className='text-white font-bold text-6xl fixed top-[51%] mx-auto max-w-max inset-x-0'>
-                <h1><span className='text-[#65DB71]'>{playerScore}</span> - {opponentScore}</h1>
-            </div>
-            <div className='text-white font-bold text-2xl md:text-3xl fixed top-[66%] mx-auto max-w-max inset-x-0'>
-                <p><span className='text-[#FFC700]'>{opponentName}</span> AND THEIR FOLLOWERS WILL NOW FOLLOW YOU!</p>
-            </div>
-            <div className='text-white font-bold text-2xl md:text-3xl fixed top-[77%] mx-auto max-w-max inset-x-0'>
-                <p>WAITING FOR THE NEXT ROUND...</p>
-            </div>
-            <div className='block md:hidden'>
-                <PlayerName2 name={playerName}/>
-            </div>
-            <div className='hidden md:block'>
-                <StarsInfoPlayer playerName={playerName} score={playerScore} />
-            </div>
-            <div className='hidden md:block'>
-                <SpectatorCount count={spectatorCount}/>
-            </div>
-        </div>
-    );
+const MatchWinScreen = ({
+  playerScore = 3,
+  opponentScore = 2,
+  playerName = "SPONGEBOB",
+  opponentName = "PATRICK",
+  spectatorCount = 32,
+}: MatchWinScreenProps) => {
+  return (
+    <div>
+      <div className="block md:hidden">
+        <PlayerName1 name={opponentName} />
+      </div>
+      <div className="hidden md:block">
+        <StarsInfoOpponent opponentName={opponentName} score={opponentScore} />
+      </div>
+      <div className="fixed top-[5%] mx-auto max-w-max inset-x-0 scale-[0.85] sm:scale-[0.85] md:scale-[0.875] lg:scale-[0.90]">
+        <img src={plainTrophy} alt="PlainTrophy" />
+      </div>
+      <div className="text-white font-bold text-6xl fixed top-[39%] mx-auto max-w-max inset-x-0">
+        <h1>YOU WON!</h1>
+      </div>
+      <div className="text-white font-bold text-6xl fixed top-[51%] mx-auto max-w-max inset-x-0">
+        <h1>
+          <span className="text-[#65DB71]">{playerScore}</span> -{" "}
+          {opponentScore}
+        </h1>
+      </div>
+      <div className="text-white font-bold text-2xl md:text-3xl fixed top-[66%] mx-auto max-w-max inset-x-0">
+        <p>
+          <span className="text-[#FFC700]">{opponentName}</span> AND THEIR
+          FOLLOWERS WILL NOW FOLLOW YOU!
+        </p>
+      </div>
+      <div className="text-white font-bold text-2xl md:text-3xl fixed top-[77%] mx-auto max-w-max inset-x-0">
+        <p>WAITING FOR THE NEXT ROUND...</p>
+      </div>
+      <div className="block md:hidden">
+        <PlayerName2 name={playerName} />
+      </div>
+      <div className="hidden md:block">
+        <StarsInfoPlayer playerName={playerName} score={playerScore} />
+      </div>
+      <div className="hidden md:block">
+        <SpectatorCount count={spectatorCount} />
+      </div>
+    </div>
+  );
 };
 
 export default MatchWinScreen;
