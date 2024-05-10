7bb3d1d2a5278f5c7cee048df1ca65353225e500
Patrick Edwards <pat.e1@icloud.com>
2024-05-11 02:42:45 +1000

    move reaction array into reaction component

--- client/src/components/ReactionsOverlay/ReactionsOverlay.tsx
+++ client/src/components/ReactionsOverlay/ReactionsOverlay.tsx
@@ -1,11 +1,40 @@
 import Reaction from "./Reaction.tsx";
 import { type ReactionProperties } from "../../types";
+import { useEffect, useState } from "react";
 
-interface ReactionOverlayProps {
-  reactions: ReactionProperties[];
-}
+function ReactionOverlay() {
+  const [reactions, setReactions] = useState<ReactionProperties[]>([]);
 
-function ReactionOverlay({ reactions }: ReactionOverlayProps) {
+  // Test reaction overlay
+  const availableEmojis = ["🎉", "💀", "😂", "❤️", "🐐"];
+  const reactionPlaceholders = [...Array(100)].map(() => {
+    return {
+      x: Math.random() * 100,
+      y: Math.random() * 100,
+      value:
+        availableEmojis[Math.floor(Math.random() * availableEmojis.length)],
+    };
+  });
+
+  useEffect(() => {
+    let reactionCounter = 0;
+    const handle = setInterval(() => {
+      reactionCounter++;
+      setReactions((reactions) => [
+        ...reactions,
+        reactionPlaceholders[reactionCounter],
+      ]);
+      if (reactionCounter === reactionPlaceholders.length - 1) {
+        reactionCounter = 0;
+      }
+    }, Math.random() * 500);
+
+    return () => {
+      setReactions([]);
+      clearInterval(handle);
+    };
+  }, []);
+
   return (
     <div className="w-screen h-screen top-0 left-0 fixed z-50">
       {reactions.map(({ x, y, value }) => (
--- client/src/pages/gameRoundScreen.tsx
+++ client/src/pages/gameRoundScreen.tsx
@@ -1,43 +1,10 @@
-import { useEffect, useState } from "react";
 import ReactionOverlay from "../components/ReactionsOverlay/ReactionsOverlay";
 import SemiCircle3dots from "../components/semiCircle3dots";
-import { ReactionProperties } from "../types";
 
 const GameRoundScreen = () => {
-  const [reactions, setReactions] = useState<ReactionProperties[]>([]);
-
-  const availableEmojis = ["🎉", "💀", "😂", "❤️", "🐐"];
-  const reactionPlaceholders = [...Array(100)].map(() => {
-    return {
-      x: Math.random() * 100,
-      y: Math.random() * 100,
-      value:
-        availableEmojis[Math.floor(Math.random() * availableEmojis.length)],
-    };
-  });
-
-  useEffect(() => {
-    let reactionCounter = 0;
-    const handle = setInterval(() => {
-      reactionCounter++;
-      setReactions((reactions) => [
-        ...reactions,
-        reactionPlaceholders[reactionCounter],
-      ]);
-      if (reactionCounter === reactionPlaceholders.length - 1) {
-        reactionCounter = 0;
-      }
-    }, Math.random() * 500);
-
-    return () => {
-      setReactions([]);
-      clearInterval(handle);
-    };
-  }, []);
-
   return (
     <>
-      <ReactionOverlay reactions={reactions} />
+      <ReactionOverlay />
       <div className="flex justify-center items-start">
         <div>
           <SemiCircle3dots variant="text" text="make your move" angle="180" />
