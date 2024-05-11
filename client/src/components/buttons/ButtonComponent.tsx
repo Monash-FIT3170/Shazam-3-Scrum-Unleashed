fb8397bc2290f290a9c5ec0df5290ac62f9b652e
RoryTU <rorytu@gmail.com>
2024-05-11 15:17:53 +1000

    feature: improve home page UI and refactored code into components (#47)

---
+++ client/src/components/buttons/ButtonComponent.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+type ButtonInputs = {
+  linkPath: string;
+  text: string;
+};
+
+const ButtonComponent = ({ linkPath, text }: ButtonInputs) => {
+  return (
+    <div className="h-14 mt-8">
+      <Link to={linkPath}>
+        <button className="text-white bg-primary text-3xl w-80 md:w-96 lg:w-122 font-bold rounded-xl h-full border-white">
+          {text}
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ButtonComponent;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,27 +1,23 @@
-import { Link } from "react-router-dom";
 import DisplayLogo from "../components/DisplayLogo";
 import { HOST_GAME_PATH, JOIN_GAME_PATH } from "./pagePaths.ts";
+import ButtonComponent from "../components/buttons/ButtonComponent.tsx";
+import BorderedButtonComponent from "../components/buttons/BorderedButtonComponent.tsx";
 
 const Home = () => {
   return (
     <div>
-      <div>
+      <div className="items-center">
         <DisplayLogo />
-      </div>
-
-      <div className="w-screen h-12 mt-8">
-        <Link to={JOIN_GAME_PATH}>
-          <button className="text-white bg-primary text-2xl font-bold w-1/3 rounded-xl h-full">
-            Join Game
-          </button>
-        </Link>
-      </div>
-      <div className="w-screen h-12 mt-4">
-        <Link to={HOST_GAME_PATH}>
-          <button className="text-white bg-primary text-2xl font-bold w-1/3 rounded-xl h-full">
-            Host Game
-          </button>
-        </Link>
+        <div className="mt-20">
+          <ButtonComponent
+            linkPath={JOIN_GAME_PATH}
+            text={"JOIN GAME"}
+          ></ButtonComponent>
+          <BorderedButtonComponent
+            linkPath={HOST_GAME_PATH}
+            text={"CREATE GAME"}
+          ></BorderedButtonComponent>
+        </div>
       </div>
     </div>
   );
