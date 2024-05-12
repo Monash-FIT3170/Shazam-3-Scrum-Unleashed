9ecd1137e0e407de19dc6716692d7f06c14fca91
Victor <vhua0006@student.monash.edu>
2024-05-12 21:22:21 +1000

    feature: adding remaining player bar

--- client/src/pages/TournamentScreen.tsx
+++ client/src/pages/TournamentScreen.tsx
@@ -7,16 +7,20 @@ const TournamentScreen = () => {
   // placeholder players
   const player = new Player("1", "mmmmmmmm", 1, false);
   const player2 = new Player("1", "wwwwwwww", 2, false);
-  const players = [player, player2];
+  const players = [player, player2, new Player("1", "wwwwwwww", 2, false), 
+  new Player("1", "wwwwwwww", 3, false), new Player("1", "wwwwwwww", 4, false),
+  new Player("1", "wwwwwwww", 5, false), new Player("1", "wwwwwwww", 6, false),
+  new Player("1", "wwwwwwww", 7, false), new Player("1", "wwwwwwww", 8, false)
+  ];
 
   return (
     <div>
       <div>
         <DisplayLogo />
       </div>
-      <h1 className="text-white font-bold mt-6 uppercase">
-        Tournament
-      </h1>
+      <br></br>
+      <div className="player-bar"><h1>Remaining Players: {players.length}</h1></div>
+      <br></br>
       <div className="player-list">
         {/* creating a playercard component for each player */}
         {players.map((player) => (
