70c5042383c4ef1ab62acf0ffbe3149ce0656070
RoryTU <rorytu@gmail.com>
2024-05-13 14:01:29 +1000

    bugfix: removed actor setters.

--- server/model/actors/actor.ts
+++ server/model/actors/actor.ts
@@ -8,8 +8,4 @@ export default abstract class Actor {
   // getters
   public getSocketID(): string { return this.socketId; }
   public getName(): string { return this.name; }
-
-  // setters
-  public setSocketID(socketId: string) { this.socketId = socketId; }
-  public setName(name: string): void { this.name = name; }
 }
