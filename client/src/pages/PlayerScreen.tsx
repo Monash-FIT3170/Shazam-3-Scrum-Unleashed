1fdaf663cea332ff6981024a10037582b06b58ae
Levi Kogan <128117470+lkog77@users.noreply.github.com>
2024-05-13 18:43:19 +1000

    bugfix: fixed components to be responsive to mobile views (#54)
    Co-authored-by: RoryTU <rorytu@gmail.com>

--- client/src/pages/PlayerScreen.tsx
+++ client/src/pages/PlayerScreen.tsx
@@ -1,20 +1,24 @@
 import DisplayLogo from "../components/DisplayLogo";
-//import WaitingToStart from "../components/WaitingToStart";
-import ChoosePlayerMove from "../components/ChoosePlayerMove";
-import CountDownTimer from "../components/CountDownTimer";
+import WaitingToStart from "../components/WaitingToStart";
+//import ChoosePlayerMove from "../components/ChoosePlayerMove";
+//import CountDownTimer from "../components/CountDownTimer";
 
 const PlayerScreen = () => {
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-hidden h-screen relative">
       <div className="pt-12">
-        <DisplayLogo />
-        {/* <div className="flex flex-col items-center justify-center mt-16"> */}
-        {/* <div className="mt-20">
+        <div className=" items-center size-60 w-full">
+          <DisplayLogo />
+        </div>
+
+        <div className="flex flex-col items-center justify-center mt-10">
+          <div className="mt-20">
             <WaitingToStart />
-          </div> */}
-        <div className="mt-20">
+          </div>
+          {/* <div className="mt-20">
           <CountDownTimer />
           <ChoosePlayerMove />
+        </div> */}
         </div>
       </div>
     </div>
