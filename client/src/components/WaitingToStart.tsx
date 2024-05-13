74ea36bda01a62f906eb245a17ba0b35c33b5ce3
lkog0002 <lkog0002@student.monash.edu>
2024-05-13 14:24:14 +1000

    Fixed WaitingToStart and PlayerScreen components to be more responsive to mobile views.

--- client/src/components/WaitingToStart.tsx
+++ client/src/components/WaitingToStart.tsx
@@ -2,20 +2,20 @@ const WaitingToStart = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       {/* Large text in the center */}
-      <h1 className="text-white text-5xl font-bold mb-80 ">
-        WAITING FOR HOST TO START THE GAME...
+      <h1 className="text-white md:text-5xl text-4xl font-bold ">
+        WAITING FOR HOST...
       </h1>
 
       {/* Text in the bottom left */}
-      <div className="fixed bottom-30 left-20 xl:left-10">
-        <p className="text-white text-lg font-bold mb-2">YOUR NICKNAME:</p>
-        <p className="text-white text-3xl font-bold">{"Johnny"}</p>
+      <div className="fixed bottom-10 md:left-20 left-5">
+        <p className="text-white md:text-lg text-sm font-bold mb-2">YOUR NICKNAME:</p>
+        <p className="text-white md:text-3xl font-bold">{"Johnny"}</p>
       </div>
 
       {/* Text in the bottom right */}
-      <div className="fixed bottom-30 right-20">
-        <p className="text-white text-lg font-bold mb-2">TOURNAMENT CODE:</p>
-        <p className="text-white text-3xl font-bold">{"000000"}</p>
+      <div className="fixed bottom-10 md:right-20 right-5">
+        <p className="text-white md:text-lg text-sm font-bold mb-2">TOURNAMENT CODE:</p>
+        <p className="text-white md:text-3xl font-bold">{"000000"}</p>
       </div>
     </div>
   );
