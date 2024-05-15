7064834b65dee4a2df705ecf85acd410115cbcc7
RoryT-U <rorytu@gmail.com>
2024-05-15 10:06:51 +1000

    feature: added a fetch that will get a QR code from https://goqr.me/api/doc/

--- client/src/pages/GameLobby.tsx
+++ client/src/pages/GameLobby.tsx
@@ -6,7 +6,9 @@ import { PlayerAttributes } from "../../../types/types.ts";
 import { socket } from "../App.tsx";
 
 const GameLobby = () => {
-  const gameData = useLoaderData() as { gameCode: string; qrCode: string };
+  const gameData = useLoaderData() as { gameCode: string, qrCode :Blob };
+
+  const img = URL.createObjectURL(gameData.qrCode);
 
   const [players, setPlayers] = useState(new Array<PlayerAttributes>());
 
@@ -27,19 +29,21 @@ const GameLobby = () => {
   }, []);
 
   return (
-    <div>
       <div>
-        <DisplayLogo />
+        <div>
+          <DisplayLogo/>
+        </div>
+        <h1 className="text-white font-bold mt-6 uppercase">
+          Game Code : {gameData.gameCode}
+          <ul>
+            {players.map((player) => (
+                <li key={player.id}>{player.name}</li>
+            ))}
+          </ul>
+
+        </h1>
+        <img src={img} alt="QR Code"/>
       </div>
-      <h1 className="text-white font-bold mt-6 uppercase">
-        Game Code : {gameData.gameCode}
-        <ul>
-          {players.map((player) => (
-            <li key={player.id}>{player.name}</li>
-          ))}
-        </ul>
-      </h1>
-    </div>
   );
 };
 
