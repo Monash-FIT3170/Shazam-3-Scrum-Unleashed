a8998a8985843a3adb18a2f0bc9b4ebd435a6d5f
Patrick Edwards <pat.e1@icloud.com>
2024-05-15 21:38:04 +1000

    remove reaction overlay

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,12 +2,10 @@ import { CREATE_GAME_PATH, JOIN_GAME_PATH } from "./pagePaths.ts";
 import ButtonComponent from "../components/buttons/ButtonComponent.tsx";
 import BorderedButtonComponent from "../components/buttons/BorderedButtonComponent.tsx";
 import DisplayLogo from "../components/DisplayLogo.tsx";
-import ReactionOverlay from "../components/reactions/ReactionsOverlay.tsx";
 
 const Home = () => {
   return (
     <div>
-      <ReactionOverlay />
       <div className="h-64 md:h-80 lg:h-96 mt-20 ">
         <DisplayLogo />
       </div>
