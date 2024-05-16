0c442d4167591febdb420ab0fbc030b88b91f685
RoryTU <rorytu@gmail.com>
2024-05-16 16:47:22 +1000

    feature: added qr codes to game lobby ui (#61)
    Co-authored-by: FuzzyLogic123 <73590996+FuzzyLogic123@users.noreply.github.com>

--- client/src/pages/GameLobby.tsx
+++ client/src/pages/GameLobby.tsx
@@ -4,11 +4,33 @@ import { useLoaderData } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { PlayerAttributes } from "../../../types/types.ts";
 import { socket } from "../App.tsx";
+import { BASE_PATH, JOIN_GAME_PATH } from "./pagePaths.ts";
+
+async function fetchQrCode(
+  returnUrl: string,
+  setQrCode: (qrCode: string) => void,
+) {
+  const qrcode = await fetch(
+    // TODO: Make this an environment variable
+    "http://localhost:3010/qr-code/" + encodeURIComponent(returnUrl),
+  );
+  const qrCode = await qrcode.json();
+  setQrCode(qrCode.qrCode);
+}
 
 const GameLobby = () => {
-  const gameData = useLoaderData() as { gameCode: string; qrCode: string };
-
+  const gameData = useLoaderData() as { gameCode: string };
   const [players, setPlayers] = useState(new Array<PlayerAttributes>());
+  const [qrCode, setQrCode] = useState("");
+
+  useEffect(
+    () =>
+      void fetchQrCode(
+        `${window.location.origin}/${BASE_PATH}/${JOIN_GAME_PATH}?gameCode=${gameData.gameCode}`,
+        setQrCode,
+      ),
+    [],
+  );
 
   const updateList = (player: PlayerAttributes) => {
     setPlayers((previousPlayers) => [...previousPlayers, player]);
@@ -49,6 +71,11 @@ const GameLobby = () => {
           Allocate Players
         </button>
       </h1>
+      {qrCode === "" ? (
+        <span>loading...</span>
+      ) : (
+        <img src={qrCode} alt="QR Code" />
+      )}
     </div>
   );
 };
