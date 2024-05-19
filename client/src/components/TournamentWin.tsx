407faa7a166ed8a562d7f57c839a85c545284b75
Jovan Vales <jval0006@student.monash.edu>
2024-05-20 04:16:48 +1000

    reduced font size on mobile

--- client/src/components/TournamentWin.tsx
+++ client/src/components/TournamentWin.tsx
@@ -21,17 +21,17 @@ const TournamentWin = ({ playerName }: TournamentWinScreenProps) => {
         />
       </div>
       <div className="flex justify-center transform">
-        <img src={star} alt="goldenWinnerCup" className="w-60" />
+        <img src={star} alt="goldenWinnerCup" className="md:w-60 hidden  md:visible" />
         <div className="text-center mt-10">
-          <h2 className="text-white text-9xl font-bold">{playerName}</h2>
-          <h2 className="text-white text-8xl font-bold">Winner</h2>
+          <h2 className="text-white md:text-8xl text-6xl font-bold">{playerName}</h2>
+          <h2 className="text-white md:text-7xl text-5xl font-bold">Winner</h2>
         </div>
-        <img src={star} alt="goldenWinnerCup" className="w-60" />
+        <img src={star} alt="goldenWinnerCup" className="md:w-60 hidden  md:visible" />
       </div>
       <div className="absolute inset-x-0 bottom-10">
-        <button className="text-white bg-primary text-3xl w-80 md:w-96 lg:w-122 font-bold rounded-xl h-full border-white">
+        {/* <button className="text-white bg-primary text-3xl w-80 md:w-96 lg:w-122 font-bold rounded-xl h-full border-white">
           {"Host Another"}
-        </button>
+        </button> */}
       </div>
     </div>
   );
