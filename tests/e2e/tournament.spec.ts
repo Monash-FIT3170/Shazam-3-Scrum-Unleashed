1dfda5725b1b013343adbb4fd489a5bdee7c76a2
Patrick Edwards <pat.e1@icloud.com>
2024-05-27 01:25:50 +1000

    test speed of CI

--- tests/e2e/tournament.spec.ts
+++ tests/e2e/tournament.spec.ts
@@ -87,7 +87,7 @@ test.describe("Tournament Game Automation", () => {
 
     const existingNames = new Set<string>();
     const joinPromises: Promise<Page>[] = [];
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 3; i++) {
       joinPromises.push(
         joinGame(gameCode as string, browser, existingNames, i + 1)
       );
