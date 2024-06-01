01b7ceb33281570000453b6da23fe1b701339fb2
Patrick Edwards <pat.e1@icloud.com>
2024-06-01 22:51:21 +1000

    add linter script to tests

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:5173/",
-    
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     navigationTimeout: 0,
