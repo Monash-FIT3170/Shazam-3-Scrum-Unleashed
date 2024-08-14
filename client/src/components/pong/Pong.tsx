a613aa21245c7a9f9824dadeb311a9f6c9d10e4f
Patrick Edwards <pat.e1@icloud.com>
2024-08-14 22:02:40 +1000

    clamp ball to paddle height

--- client/src/components/pong/Pong.tsx
+++ client/src/components/pong/Pong.tsx
@@ -13,11 +13,50 @@ type PongProps = {
   isPlayerOne: boolean;
 };
 
-const clampX = (number: number, ballRadius: number = 0, gameWidth: number) =>
+const clampX = (number: number, gameWidth: number) =>
   Math.min(
-    Math.max(number, 0 + ballRadius * SCALING_FACTOR),
-    gameWidth - ballRadius * SCALING_FACTOR
+    Math.max(number, 0 + BALL_RADIUS * SCALING_FACTOR),
+    gameWidth - BALL_RADIUS * SCALING_FACTOR
   );
+
+const clampY = (
+  number: number,
+  paddlePosition: PongPaddleState | undefined,
+  ballState: PongBallState
+) => {
+
+  if (!paddlePosition) {
+    return number;
+  }
+
+  console.log(paddlePosition.y);
+  console.log(number);
+
+  if (
+    number < paddlePosition.y + BALL_RADIUS * SCALING_FACTOR &&
+    ballState.y < 50 * SCALING_FACTOR
+  ) {
+    return paddlePosition.y + BALL_RADIUS * SCALING_FACTOR;
+  } else if (
+    number > paddlePosition.y - BALL_RADIUS * SCALING_FACTOR &&
+    ballState.y > 50 * SCALING_FACTOR
+  ) {
+    return paddlePosition.y - BALL_RADIUS * SCALING_FACTOR;
+  }
+
+  // if (
+  //   ballState.x + BALL_RADIUS * SCALING_FACTOR >
+  //     paddlePosition.x - paddlePosition.width / 2 &&
+  //   ballState.x - BALL_RADIUS * SCALING_FACTOR <
+  //     paddlePosition.x + paddlePosition.width / 2
+  // ) {
+  //   return number > 50 * SCALING_FACTOR
+  //     ? paddlePosition.y - BALL_RADIUS * SCALING_FACTOR
+  //     : paddlePosition.y + BALL_RADIUS * SCALING_FACTOR;
+  // }
+
+  return number;
+};
 
 const Pong = ({ tournamentCode, isPlayerOne }: PongProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -46,7 +85,7 @@ const Pong = ({ tournamentCode, isPlayerOne }: PongProps) => {
       ...serverPaddleStates[0],
       width: serverPaddleStates[0].width * SCALING_FACTOR,
       x: serverPaddleStates[0].x * SCALING_FACTOR,
-      y: serverPaddleStates[0].y * SCALING_FACTOR - PADDLE_HEIGHT,
+      y: serverPaddleStates[0].y * SCALING_FACTOR,
     };
     paddle2Position.current = {
       ...serverPaddleStates[1],
@@ -62,6 +101,7 @@ const Pong = ({ tournamentCode, isPlayerOne }: PongProps) => {
     ctx.fillStyle = "#22026c";
     ctx.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
+    
     // Center line
     ctx.strokeStyle = "rgba(255, 255, 255, 0.5)";
     ctx.setLineDash([5, 5]);
@@ -72,18 +112,26 @@ const Pong = ({ tournamentCode, isPlayerOne }: PongProps) => {
     ctx.setLineDash([]);
 
     // Ball
+    const xClamped = clampX(ballState.current.x, GAME_WIDTH);
+    const closestPaddle =
+      ballState.current.y > 50 * SCALING_FACTOR
+        ? paddle2Position
+        : paddle1Position;
+    const yClamped = clampY(
+      ballState.current.y,
+      closestPaddle.current,
+      ballState.current
+    );
+
+    const strokeWidth = 2;
+    const adjustedRadius = BALL_RADIUS * SCALING_FACTOR - strokeWidth;
+
     ctx.fillStyle = "#ff00ff";
     ctx.beginPath();
-    ctx.arc(
-      ballState.current.x,
-      ballState.current.y,
-      BALL_RADIUS * SCALING_FACTOR,
-      0,
-      Math.PI * 2
-    );
+    ctx.arc(xClamped, yClamped, adjustedRadius, 0, Math.PI * 2);
     ctx.fill();
     ctx.strokeStyle = "white";
-    ctx.lineWidth = 2;
+    ctx.lineWidth = strokeWidth;
     ctx.stroke();
 
     // Paddles
@@ -92,22 +140,37 @@ const Pong = ({ tournamentCode, isPlayerOne }: PongProps) => {
       y: number,
       width: number,
       height: number,
-      color: string
+      color: string,
+      isTop = false
     ) => {
+      const strokeWidth = 2;
+      const adjustedX = x + strokeWidth / 2;
+      const adjustedY = y + strokeWidth / 2;
+      const adjustedWidth = width - strokeWidth;
+      const adjustedHeight = height - strokeWidth;
+      const topOffset = isTop ? -PADDLE_HEIGHT : 0;
+
       ctx.fillStyle = color;
-      ctx.fillRect(x, y, width, height);
+      ctx.fillRect(x, y + topOffset, width, height);
       ctx.strokeStyle = "white";
-      ctx.lineWidth = 2;
-      ctx.strokeRect(x, y, width, height);
+      ctx.lineWidth = strokeWidth;
+      ctx.strokeRect(
+        adjustedX,
+        adjustedY + topOffset,
+        adjustedWidth,
+        adjustedHeight
+      );
     };
 
+    // TODO - Have paddles drawn in middle at the start by default
     if (paddle1Position.current) {
       drawPaddle(
         paddle1Position.current.x,
         paddle1Position.current.y,
         paddle1Position.current.width,
         PADDLE_HEIGHT,
-        "#ff4757"
+        "#ff4757",
+        true
       );
     }
 
@@ -131,14 +194,9 @@ const Pong = ({ tournamentCode, isPlayerOne }: PongProps) => {
     const deltaTime = (now - lastUpdateTime.current) / 1000;
     lastUpdateTime.current = now;
 
-    const xClamped = clampX(
-      ballState.current.x + ballState.current.xVelocity * deltaTime,
-      BALL_RADIUS,
-      GAME_WIDTH
-    );
     ballState.current = {
       ...ballState.current,
-      x: xClamped,
+      x: ballState.current.x + ballState.current.xVelocity * deltaTime,
       y: ballState.current.y + ballState.current.yVelocity * deltaTime,
     };
 
