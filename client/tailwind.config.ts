a40d0ef0fca24645d1a8fa560c19831cdc730823
SANDY_SENG <ssen0027@student.monash.edu>
2024-08-22 05:59:07 +1000

    Created duel in progress animation

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -18,6 +18,76 @@ export default {
         "saturated-red": "#E943E9",
         "bright-red": "#FF0000",
       },
+      keyframes: {
+        // ROCK, PAPER text keyframes
+        customping: {
+          from: {
+            opacity: "0",
+          },
+          "50%": {
+            opacity: "1",
+            transform: "scale(1.8)"
+          },
+          to: {
+            opacity: "0",
+          }
+        },
+        // SHAZAM text img keyframes
+        customping2: {
+          from: {
+            opacity: "0",
+          },
+          "50%": {
+            opacity: "1",
+            transform: "scale(1.75)"
+          },
+          to: {
+            opacity: "1",
+            transform: "scale(1.75)"
+          }
+          // to: {
+          //   opacity: "0",
+          // }
+        },
+        // explosion keyframes
+        customping3: {
+          from: {
+            opacity: "1",
+          },
+          to: {
+            opacity: "1",
+            transform: "scale(30)"
+          }
+        },
+        // hands punching in from bottom keyframes
+        translateinbottom: {
+          from: {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
+            transform: "translate(0, 20%)"
+          }
+        },
+        // hands punching in from top keyframes
+        translateintop: {
+          from: {
+            opacity: "0",
+            transform: "scaleY(-1) translate(0, 100%)"
+          },
+          to: {
+            opacity: "1",
+            transform: "scaleY(-1) translate(0, 20%)",
+          },
+        }
+      },
+      animation: {
+        customping: "customping 1s ease-out var(--customping-delay, 0) forwards",
+        customping2: "customping2 1s ease-out var(--customping2-delay, 0) forwards",
+        customping3: "customping3 1.5s ease-out var(--customping3-delay, 0) forwards",
+        translateinbottom: "translateinbottom 0.5s ease-out var(--translateinbottom-delay, 0) forwards",
+        translateintop: "translateintop 0.5s ease-out var(--translateintop-delay, 0) forwards"
+      },
     },
   },
   plugins: [],
