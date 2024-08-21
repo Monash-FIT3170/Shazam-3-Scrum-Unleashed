2af7ed4ae7ec7cc3f7cd4b37cc131fadabe32a6c
lkog0002 <lkog0002@student.monash.edu>
2024-08-21 11:09:12 +1000

    Backend checks for players before starting tournament

--- server/src/controllers/http/startTournamentHandler.ts
+++ server/src/controllers/http/startTournamentHandler.ts
@@ -22,11 +22,6 @@ export async function startTournamentHandler(
     return;
   }
 
-  if (!player || player.length < 2) {
-    res.sendStatus(400);
-    return;
-  }
-
   await roundInitialiser(tournament, io);
   res.sendStatus(200);
 }
