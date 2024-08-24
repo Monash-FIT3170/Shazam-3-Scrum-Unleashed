bbb0761ef3b7571ca88b555797c7ea34bdd980d1
AN4RCH1ST <140788353+AN4RCH1ST@users.noreply.github.com>
2024-08-24 20:20:58 +1000

    Added a back button on create tournament screen (#120)

--- client/src/pages/GameMenu.tsx
+++ client/src/pages/GameMenu.tsx
@@ -2,10 +2,14 @@ import BorderedButtonComponent from "../components/buttons/BorderedButtonCompone
 import LargeButton from "../components/buttons/LargeButton";
 import RockPaperPong from "../assets/logo/RockPaperPong.svg";
 import LogoForMenu from "../assets/logo/LogoForMenu.svg";
+import ButtonComponent from "../components/buttons/BorderedButtonComponent.tsx";
 
 const GameMenu = () => {
   return (
     <div className="px-8">
+      <div className="fixed top-0 md:right-20 right-5">
+        <ButtonComponent linkPath="/" text={"Back"} />
+      </div>
       <div className="flex justify-center mt-28 space-x-20">
         <LargeButton linkPath="/create-tournament" image={RockPaperPong} />
         <LargeButton linkPath="/create-tournament" image={LogoForMenu} />
