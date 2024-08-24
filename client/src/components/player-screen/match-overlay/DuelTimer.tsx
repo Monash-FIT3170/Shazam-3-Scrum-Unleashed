d58f205185fb715266b0b41885a39b3281e3364d
James Chea <jche0346@student.monash.edu>
2024-08-24 16:00:47 +1000

    Redid the DuelTimer code to only be responsible for displaying the time

--- client/src/components/player-screen/match-overlay/DuelTimer.tsx
+++ client/src/components/player-screen/match-overlay/DuelTimer.tsx
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-
-// TODO: Get socket ID for the room
-const socket = io('http://localhost:3001');
-
-export const Timer = () => {
-    const [duelTime, setDuelTime] = useState(30);
-
-    useEffect(() => {
-        // Starting round countdown
-        socket.on('startDuelTimer', (time: number) => {
-            setDuelTime(time);
-        });
-
-        // Ending the round countdown
-        socket.on('stopDuelTimer', () => {
-            console.log("Duel Ended")
-            setDuelTime(0);
-        });
-        
-        // Cleaning up event listeners
-        return () => {
-            socket.off('startDuelTimer');
-            socket.off('stopDuelTimer');
-        };
-    }, []);
-
-    return (
-        <div>
-            <h1>Duel Time: {duelTime}</h1>
-        </div>
-    )
+type duelTimerProps = {
+    time: number;
 }
+
+const DuelTimer = ({time}: duelTimerProps ) => {
+    return (
+        <div className="hidden md:block">
+            <div className="fixed text-white font-bold text-2xl px-8 rounded-2xl">
+                <h2>Timer: {time}</h2>
+            </div>
+        </div>
+    );
+};
+
+export default DuelTimer;
