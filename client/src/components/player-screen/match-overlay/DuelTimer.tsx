96a299fdf632a9c02b8a9220795471a031439711
James Chea <jche0346@student.monash.edu>
2024-08-25 18:44:06 +1000

    Ran linting

--- client/src/components/player-screen/match-overlay/DuelTimer.tsx
+++ client/src/components/player-screen/match-overlay/DuelTimer.tsx
@@ -9,8 +9,11 @@ const DuelTimer = ({ time }: duelTimerProps) => {
     // <div className="fixed text-white font-bold px-8 rounded-2xl">
     //   <h2 className="sm:text-xl md:text-2xl">Timer: {time}</h2>
     // </div>
-    <div className="fixed left-1/16 transform -translate-y-1/2 text-white text-4xl font-bold flex items-center" style={{ top: '65%' }}>
-    <img src={Timer} alt="Timer" className="w-10 h-10 mr-4" />
+    <div
+      className="fixed left-1/16 transform -translate-y-1/2 text-white text-4xl font-bold flex items-center"
+      style={{ top: "65%" }}
+    >
+      <img src={Timer} alt="Timer" className="w-10 h-10 mr-4" />
       {time}
     </div>
   );
