0f75cd5a76b49f1fbb0566927f9cc9c00c2fd988
tom-rumble <tomrumble414@gmail.com>
2024-08-25 22:19:52 +1000

    feat: add optional classname prop to btn+input

--- client/src/components/buttons/FormButton.tsx
+++ client/src/components/buttons/FormButton.tsx
@@ -2,12 +2,13 @@ type FormButtonInputs = {
   text: string;
   loading: boolean;
   callback: () => void;
+  additionalClass?: string;
 };
 
-const FormButtonComponent = ({ text, loading, callback }: FormButtonInputs) => {
+const FormButtonComponent = ({ text, loading, callback, additionalClass }: FormButtonInputs) => {
   return (
     <button
-      className="h-12 mt-8 text-white bg-primary text-2xl font-bold w-1/3 rounded-xl"
+      className={`h-12 mt-8 text-white bg-primary text-2xl font-bold w-1/3 rounded-xl ${additionalClass ?? ""}`}
       onClick={callback}
       disabled={loading}
     >
