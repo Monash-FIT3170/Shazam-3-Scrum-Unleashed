c182ed38470e3722b94cd1740f33cd69740c4811
James Chea <jche0346@student.monash.edu>
2024-08-25 17:38:56 +1000

    Using the UI appearance of the CountDownTimer

--- client/src/components/player-screen/match-overlay/DuelTimer.tsx
+++ client/src/components/player-screen/match-overlay/DuelTimer.tsx
@@ -1,16 +1,17 @@
+import Timer from "../../../assets/choose-move/Timer.svg";
+
 type duelTimerProps = {
   time: number;
 };
 
 const DuelTimer = ({ time }: duelTimerProps) => {
   return (
-    // <div className="hidden md:block">
-    // <div className="fixed text-white font-bold text-2xl px-8 rounded-2xl">
-    //     <h2>Timer: {time}</h2>
+    // <div className="fixed text-white font-bold px-8 rounded-2xl">
+    //   <h2 className="sm:text-xl md:text-2xl">Timer: {time}</h2>
     // </div>
-    // </div>
-    <div className="fixed text-white font-bold px-8 rounded-2xl">
-      <h2 className="sm:text-xl md:text-2xl">Timer: {time}</h2>
+    <div className="fixed top-1/2 left-1/16 transform -translate-y-1/2 text-white text-5xl font-bold flex items-center">
+    <img src={Timer} alt="Timer" className="w-10 h-10 mr-4" />
+      {time}
     </div>
   );
 };
