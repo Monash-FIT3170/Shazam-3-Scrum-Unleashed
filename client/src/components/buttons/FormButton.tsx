07acfa888ce354ad5dfaef4301148cf086edba7e
tom-rumble <tomrumble414@gmail.com>
2024-08-25 22:46:25 +1000

    clean: format code

--- client/src/components/buttons/FormButton.tsx
+++ client/src/components/buttons/FormButton.tsx
@@ -5,7 +5,12 @@ type FormButtonInputs = {
   additionalClass?: string;
 };
 
-const FormButtonComponent = ({ text, loading, callback, additionalClass }: FormButtonInputs) => {
+const FormButtonComponent = ({
+  text,
+  loading,
+  callback,
+  additionalClass,
+}: FormButtonInputs) => {
   return (
     <button
       className={`py-2 px-5 mt-8 text-white bg-primary text-2xl font-bold w-1/3 rounded-xl ${additionalClass ?? ""}`}
