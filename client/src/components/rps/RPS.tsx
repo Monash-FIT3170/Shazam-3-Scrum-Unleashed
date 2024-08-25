cfd638fda26ef0b94f33aa137abee01e02840471
RoryT-U <rorytu@gmail.com>
2024-08-25 23:22:46 +1000

    feature: fixed duel outcome screen being skipped

--- client/src/components/rps/RPS.tsx
+++ client/src/components/rps/RPS.tsx
@@ -22,18 +22,24 @@ const RPS = ({
   const [userAction, setUserAction] = useState<Action>();
   const [opponentAction, setOpponentAction] = useState<Action>();
 
+
   useEffect(() => {
     socket.on("MATCH_RPS_DUEL_STATE", (p1Action, p2Action) => {
       setUserAction(isPlayerOne ? p1Action : p2Action);
       setOpponentAction(isPlayerOne ? p2Action : p1Action);
+
+      setTimeout(() => {
+        setUserAction(undefined);
+        setOpponentAction(undefined);
+      }, 3000);
     });
+
+    return () => {
+      socket.off("MATCH_RPS_DUEL_STATE");
+    };
   }, []);
 
   if (userAction && opponentAction) {
-    setTimeout(() => {
-      setUserAction(undefined);
-      setOpponentAction(undefined);
-    }, 3000);
     return (
       <DuelOutcome
         userPlayer={player}
