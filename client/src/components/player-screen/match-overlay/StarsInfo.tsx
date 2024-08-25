5766093009883432818b4e61b91de3e43862bb67
Jovan Vales <jval0006@student.monash.edu>
2024-08-26 03:29:52 +1000

    UI improvements

--- client/src/components/player-screen/match-overlay/StarsInfo.tsx
+++ client/src/components/player-screen/match-overlay/StarsInfo.tsx
@@ -18,17 +18,17 @@ const StarsInfo = ({
   return (
     <div
       className={
-        "fixed text-white font-bold text-2xl px-8 rounded-2xl" +
-        ` ${isOpponent ? "top-5 right-6 bg-[#FF5959] pt-1 pb-3" : `bottom-5 left-6 py-2 ${isSpectator ? "bg-spectator-bg" : "bg-[#45BD67]"}`}`
+        "fixed text-white font-bold text-2xl px-8 rounded-2xl pb-2 -m-4 md:-m-0" +
+        ` ${isOpponent ? "top-6 right-6 bg-[#FF5959] pt-1 " : `bottom-6 left-6 ${isSpectator ? "bg-spectator-bg" : "bg-[#45BD67]"}`}`
       }
     >
       <p style={{ display: "inline-flex", alignItems: "center" }}>
         {isSpectator && !isOpponent ? (
           <img src={eyeIcon} alt="Eye Icon" style={{ marginRight: "8px" }} />
         ) : null}
-        {playerName}
+        {playerName} <p className="md:hidden"> {`: ${score}`}</p>
       </p>
-      <div className="flex mx-auto max-w-max inset-x-0 space-x-3 mt-1 scale-90">
+      <div className="mx-auto max-w-max inset-x-0 space-x-3 mt-1 scale-90 hidden sm:flex">
         <img src={score >= 1 ? filledStar : unfilledStar} />
         <img src={score >= 2 ? filledStar : unfilledStar} />
         <img src={score >= 3 ? filledStar : unfilledStar} />
