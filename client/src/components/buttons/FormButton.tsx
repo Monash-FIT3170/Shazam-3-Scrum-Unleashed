9e1bd9a6a699129e592cbaede41d478838e42709
tom-rumble <tomrumble414@gmail.com>
2024-08-25 22:22:59 +1000

    fix: change btn height to y padding

--- client/src/components/buttons/FormButton.tsx
+++ client/src/components/buttons/FormButton.tsx
@@ -8,7 +8,7 @@ type FormButtonInputs = {
 const FormButtonComponent = ({ text, loading, callback, additionalClass }: FormButtonInputs) => {
   return (
     <button
-      className={`h-12 mt-8 text-white bg-primary text-2xl font-bold w-1/3 rounded-xl ${additionalClass ?? ""}`}
+      className={`py-2 px-5 mt-8 text-white bg-primary text-2xl font-bold w-1/3 rounded-xl ${additionalClass ?? ""}`}
       onClick={callback}
       disabled={loading}
     >
