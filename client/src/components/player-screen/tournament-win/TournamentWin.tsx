ea6b5a0321145c0b95e2b5b94a9d68b944e36572
ssen0027 <162076763+ssen0027@users.noreply.github.com>
2024-09-14 18:30:22 +1000

    feature: slight UI improvements (#163)

--- client/src/components/player-screen/tournament-win/TournamentWin.tsx
+++ client/src/components/player-screen/tournament-win/TournamentWin.tsx
@@ -46,26 +46,28 @@ const TournamentWin = ({ playerName }: TournamentWinScreenProps) => {
       <div className="flex justify-center transform -mb-80">
         <img
           src={goldenWinnerCup}
-          alt="goldenWinnerCup"
+          alt="golden winner cup"
           className="lg:w-60 w-32 opacity-30"
         />
       </div>
       <div className="flex justify-center transform">
         <img
           src={star}
-          alt="goldenWinnerCup"
-          className="md:w-60 hidden  md:visible"
+          alt="star"
+          className="lg:w-44 w-32 hidden md:block -translate-x-16 lg:-translate-x-24"
         />
         <div className="text-center mt-52 lg:mt-10">
-          <h2 className="text-white lg:text-8xl text-4xl font-bold">
+          <h2 className="text-white lg:text-8xl text-4xl font-bold -mt-2">
             {playerName.slice(0, 15)}
           </h2>
-          <h2 className="text-white md:text-7xl text-5xl font-bold">Winner</h2>
+          <h2 className="text-white md:text-7xl text-5xl font-bold mt-2">
+            Winner
+          </h2>
         </div>
         <img
           src={star}
-          alt="goldenWinnerCup"
-          className="md:w-60 hidden  md:visible"
+          alt="star"
+          className="lg:w-44 w-32 hidden md:block translate-x-16 lg:translate-x-24"
         />
       </div>
       <div className="absolute inset-x-0 bottom-10 z-50">
