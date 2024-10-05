df1d190f52f5da94022270c29b8fb2b4f424e540
Aaron Abbott <40223320+Technolord3233@users.noreply.github.com>
2024-10-05 16:05:04 -0700

    modified powerups, and implemented the tie breaker and shield functionality (#177)
    Co-authored-by: Victor Huang <139522440+Rotciv3210@users.noreply.github.com>
    Co-authored-by: RoryTU <rorytu@gmail.com>

--- server/src/model/powerups/rpsPowerups/powerup.ts
+++ server/src/model/powerups/rpsPowerups/powerup.ts
@@ -4,20 +4,23 @@ export default abstract class Powerup {
   description: string;
   targetSelf: boolean; //whether this effect will hit the user or the opponent
   instantUse: boolean; //instant use is whether it activates immediately. Other powerups are used at the end of the round to check who won.
+  public priority: number; //higher number means higher priority. for non-instant powerups, this means the larger numbers are used later, as they will be more impactful.
 
   constructor(
     name: string,
     description: string,
     targetSelf: boolean,
     instantUse: boolean,
+    priority: number,
   ) {
     this.name = name;
     this.description = description;
     this.targetSelf = targetSelf;
     this.instantUse = instantUse;
+    this.priority = priority;
   }
 
   //child classes will need to implement this powerup method
   //this method should just take in the current rpsMatch, as this will contain all the info needed. such as the players, and the players actions.
-  abstract usePowerup(rpsMatch: RpsMatch): void;
+  abstract usePowerup(rpsMatch: RpsMatch, p1Activate: boolean): void;
 }
